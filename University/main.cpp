e9619a9bc7f6c800170a7b9db865a644158afcf1
eemule25 <75223615+eemule25@users.noreply.github.com>
2020-11-30 22:36:37 +0200

    Update main.cpp

--- University/main.cpp
+++ University/main.cpp
@@ -5,12 +5,6 @@
  * It reads user-given commands. Depending on the command,
  * the program either stores data into a suitable data structure,
  * or makes searches in the data structure in question.
- *
- * Program author:
- * Name: Eemil Määttä
- * Student number: 284283
- * UserID: maatta5
- * E-Mail: eemil.maatta@tuni.fi
  * */
 
 #include "utils.hh"
